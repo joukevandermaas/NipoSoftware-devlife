bda635c64b34db1c4f28424b0873ddb60b71e871
Bas Bakker <b.bakker@nipo.com>
2019-03-19 14:03:43 +0100

    Added door stuff

--- Assets/2D Platformer Controller/Scripts/GameManager.cs
+++ Assets/2D Platformer Controller/Scripts/GameManager.cs
@@ -10,6 +10,9 @@ public class GameManager : MonoBehaviour
     public float spawnBugChance = 0.2f;
     public float spawnBugChanceIncreaseRate = 0.025f;
 
+    public float decreaseSpawnTimeWhenBugReachesDoor = 0.2f;
+    public float decreaseSpawnTimeWhenFeatureReachesDoor = 0.2f;
+
     public void ShotsFired()
     {
         var rnd = UnityEngine.Random.Range(0f, 1f);
@@ -23,7 +26,6 @@ public class GameManager : MonoBehaviour
 
     public void BugReachedDoor()
     {
-
     }
 
     public void FeatureReachedDoor()
